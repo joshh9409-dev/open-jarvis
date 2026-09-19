0f052e33d694ebdc291256f2e5273ad48d6b6204
joshh9409-dev <joshh9409@gmail.com>
2026-09-19 04:47:35 +0100

    Add Open Jarvis color palette

---
+++ app/src/main/java/com/openjarvis/ui/theme/VoidColor.kt
@@ -0,0 +1,22 @@
+package com.openjarvis.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object VoidColor {
+    val Void950 = Color(0xFF070711)
+    val Void900 = Color(0xFF0D0D1A)
+    val Void800 = Color(0xFF151525)
+    val Void700 = Color(0xFF20203A)
+    val Void600 = Color(0xFF303050)
+    val TextPrimary = Color(0xFFF5F3FF)
+    val TextSecondary = Color(0xFFAAA6C3)
+    val TextDisabled = Color(0xFF68647C)
+    val Violet = Color(0xFF9B6CFF)
+    val VioletDim = Color(0xFF5C3FA0)
+    val Cyan = Color(0xFF55DFFF)
+    val Green = Color(0xFF55E6A5)
+    val Amber = Color(0xFFFFC857)
+    val Red = Color(0xFFFF5F7A)
+    val BorderSubtle = Color(0xFF292840)
+    val BorderGlow = Color(0xFF6650A8)
+}
